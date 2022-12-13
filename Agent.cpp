e9a766f7ee196a3fbda2bdd7f649abf6dad0d860
X <>
2022-12-12 22:44:58 -0500

    -

--- Agent.cpp
+++ Agent.cpp
@@ -2,9 +2,19 @@
 
 #include <stdio.h>
 
+#include <boost/math/distributions/negative_binomial.hpp> // for negative_binomial_distribution
+  using boost::math::negative_binomial_distribution; // default type is double.
+  using boost::math::negative_binomial; // typedef provides default type is double.
+#include <boost/math/distributions/binomial.hpp> // for binomial_distribution.
+#include <boost/math/distributions/beta.hpp> // for beta_distribution.
+#include <boost/math/distributions/gamma.hpp> // for gamma_distribution.
+#include <boost/math/distributions/normal.hpp> // for normal_distribution.
+
 namespace Sim {
     class Agent {
-        Agent();
+        Agent() {
+
+        }
 
     };
 };
---
+++ main.cpp
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+#include "Agent.h"
+
+int main() {
+
+
+}
