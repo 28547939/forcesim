ecc691d7ac20b8832c05e4682144aab7c66e7e8f
28547939 <122396215+28547939@users.noreply.github.com>
2024-01-29 23:56:25 +0000

    updated path to v2-dist-points binary

--- py/src/distgraph/generate-dist-graph.sh
+++ py/src/distgraph/generate-dist-graph.sh
@@ -3,12 +3,12 @@
 set -o nounset
 set -x
 
-BASE=../..
+BASE=../../..
 
 # generate-dist-graph.sh
 # See notes in generate-dist-graph.py
 
-$BASE/v2-dist-points --agent-config-path $BASE/py/tests/basic/agents-json/1.json  \
+$BASE/build/v2-dist-points --agent-config-path $BASE/py/tests/basic/agents-json/1.json  \
 	--agent-config-key 2023-11-19_3 --price-view 100 --current-price 1 --subjectivity-extent 0.99 \
 	| /usr/local/bin/python3.9 ./generate-dist-graph.py \
 		--distgraph-output-file ./distgraph.png \
