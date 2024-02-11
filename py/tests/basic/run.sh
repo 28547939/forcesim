c201cb9214335d69c2adbd6c94434e62739eae4c
28547939 <122396215+28547939@users.noreply.github.com>
2024-02-11 23:06:44 +0000

    revisions to readme and py/tests/basic/run.sh

--- py/tests/basic/run.sh
+++ py/tests/basic/run.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-# useful for development
-export PYTHONPATH=$PYTHONPATH:../../src
 
-./basic.py  --agents-json-dir=./agents-json \
+# useful for development
+#if [ -z $PYTHONPATH ]; then 
+#	export PYTHONPATH=../../src
+#else
+#	export PYTHONPATH=$PYTHONPATH:../../src
+#fi
+
+set -o nounset
+
+$PYTHON ./basic.py  --agents-json-dir=./agents-json \
 	--subscribers-json-dir=./subscribers-json \
 	--info-json-dir=./info-json \
 	--config-yaml=./config.yml
