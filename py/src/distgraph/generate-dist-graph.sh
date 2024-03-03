e9db8737d48b78da450ecb25cf4655133c0d2985
28547939 <28547939>
2024-03-03 02:44:49 +0000

    no_points_table option for distgraph; subjectivity-extent argument to generate-dist-graph.sh

--- py/src/distgraph/generate-dist-graph.sh
+++ py/src/distgraph/generate-dist-graph.sh
@@ -8,8 +8,11 @@ BASE=../../..
 # generate-dist-graph.sh
 # See notes in generate-dist-graph.py
 
+S=$1
+
 $BASE/build/v2-dist-points --agent-config-path $BASE/py/tests/basic/agents-json/1.json  \
-	--agent-config-key 2023-11-19_3 --price-view 100 --current-price 1 --subjectivity-extent 0.99 \
+	--agent-config-key 2023-11-19_3 --price-view 100 --current-price 1 --subjectivity-extent $S \
 	| /usr/local/bin/python3.9 ./generate-dist-graph.py \
 		--distgraph-output-file ./distgraph.png \
-		--parameters-output-file ./parameters.png
+		--parameters-output-file ./parameters.png \
+		--no-points-table
