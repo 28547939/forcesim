337b5f1bf2ad3d0fea8aba6cb6c8b702d7fa4a29
28547939 <122396215+28547939@users.noreply.github.com>
2024-08-29 13:29:56 +0000

    cpp: tests: update to info-test
    - comments and cosmetic changes (printing test data differently)
    - info-probability argument
    - off-by-1 error when adding empty entries to reference_info_history

--- tests/info.cpp
+++ tests/info.cpp
@@ -84,18 +84,34 @@ int main (int argc, char* argv[]) {
     forcesim_client client ({ forcesim_component::Market });
 
     client.options_desc.add_options()
-        ("test-count", po::value<int>()->default_value(10), "")
-        ("agent-count-max", po::value<int>()->default_value(10), "")
-        ("infoset-size-max", po::value<int>()->default_value(10), "")
-        ("info-count-max", po::value<int>()->default_value(10), "")
-        ("iter-max", po::value<int>()->default_value(10), "")
+        ("test-count", po::value<int>()->default_value(5), "")
+
+        // the likelihood that a given iterblock will have an Infoset submitted
+        ("info-probability", po::value<float>()->default_value(0.8), "")
+
+        // the following parameters are chosen randomly, up to the given max
+
+
+        // number of agents to receive Info objects
+        ("agent-count-max", po::value<int>()->default_value(1), "")
+
+        // number of Info objects to be contained in each submitted Infoset
+        ("infoset-size-max", po::value<int>()->default_value(1), "")
+
+        // at most one Infoset is submitted for each iteration block
+        ("iterblock-count-max", po::value<int>()->default_value(5), "")
+        
+        // maximum size of iteration blocks (i.e. to occur between Infoset submissions)
+        ("iter-max", po::value<int>()->default_value(5), "")
+
     ;
     client.parse_cli(argc, argv);
     auto test_count = client.options_vm["test-count"].as<int>();
     auto agent_count_max = client.options_vm["agent-count-max"].as<int>();
     auto infoset_size_max = client.options_vm["infoset-size-max"].as<int>();
-    auto info_count_max = client.options_vm["info-count-max"].as<int>();
+    auto iterblock_count_max = client.options_vm["iterblock-count-max"].as<int>();
     auto iter_max = client.options_vm["iter-max"].as<int>();
+    auto info_probability = client.options_vm["info-probability"].as<float>();
     auto m = client.market.value();
 
     std::thread client_thread([m, &client]{
@@ -142,15 +158,15 @@ int main (int argc, char* argv[]) {
 
         timepoint_t total_iter;
 
-        int info_count = rand(info_count_max);
-        std::cout << "info_count=" << std::to_string(info_count) << "\n";
+        int iterblock_count = rand(iterblock_count_max);
+        std::cout << "iterblock_count=" << std::to_string(iterblock_count) << "\n";
 
-        for (int i = 0; i < info_count; i++) {
+        for (int i = 0; i < iterblock_count; i++) {
 
             int iter = rand(iter_max);
             std::cout << "iter=" << std::to_string(iter) << "\n";
 
-            if (randtf()) {
+            if (randtf(info_probability)) {
                 Info::infoset_t is;
                 for (int j = 0; j < rand(infoset_size_max); j++) {
                     auto info_ptr = std::make_shared<Info::Info<Info::Types::Test1>>();
@@ -172,7 +188,7 @@ int main (int argc, char* argv[]) {
             m->queue_op(op);
 
             // no info is emitted during the iteration - not possible with current Market implementation
-            for (int j = 0; j < iter; j++) {
+            for (int j = 0; j < iter-1; j++) {
                 reference_info_history.info_history.push_back(std::nullopt);
             }
 
@@ -183,25 +199,31 @@ int main (int argc, char* argv[]) {
         auto rsize = ref_history.size();
         std::cout << "reference history size: " << rsize << "\n";
 
+        print_infohistory(reference_info_history, std::nullopt);
+        std::cout << "\n\n";
+
         for (auto [id, hptr] : agent_info_history) {
             auto hsize = hptr->info_history.size();
             std::cout 
                 << "agent_info_history[" << id.to_string() 
                 << "] size: " << hsize << "\n";
 
+
             if (hsize != rsize) {
                 std::cout << "sizes differ\n";
-                print_infohistory(reference_info_history, std::nullopt);
                 print_infohistory(*hptr, id);
+                std::cout << "\n\n";
                 continue;
             }
 
+
+            print_infohistory(*hptr, id);
             for (int i = 0; i < ref_history.size(); i++) {
                 if (hptr->info_history.at(i) != ref_history.at(i)) {
                     std::cout << "not equal at " << i << "\n";
 
-                    print_infohistory(reference_info_history, std::nullopt);
                     print_infohistory(*hptr, id);
+                    std::cout << "\n\n";
                     continue;
                 }
 
